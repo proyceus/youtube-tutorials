d5a638a99c23e2abb5742fb2fba70189134fa758
proyceus <proyceus@gmail.com>
2022-04-06 17:55:54 -0400

    added in video playback and back button

--- Netflix-Starter/src/pages/Player.js
+++ Netflix-Starter/src/pages/Player.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
+import { Icon } from 'web3uikit';
+import "./Player.css";
 
 const Player = () => {
+
+  const {state: currentlyPlaying} = useLocation();
   
   return (
   <>
-  <div className="container">
-  <Link to="/" className="link">Home</Link>
+  <div className="playerPage">
+  <video autoPlay controls className="videoPlayer">
+      <source src={currentlyPlaying} type="video/mp4"></source>
+    </video>
+
+    <div className="backHome">
+      <Link to="/">
+        <Icon
+          className="backButton"
+          fill="rgba(255,255,255,0.25)"
+          size={60}
+          svg="arrowCircleLeft"
+          />
+      </Link>
+    </div>
   </div>
+    
   </>
 )
 }

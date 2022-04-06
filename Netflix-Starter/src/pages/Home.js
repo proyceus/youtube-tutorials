1a59d14bd40a253ec02f9bfda680c48a482f8a21
proyceus <proyceus@gmail.com>
2022-04-06 16:05:00 -0400

    starting project

--- Netflix-Starter/src/pages/Home.js
+++ Netflix-Starter/src/pages/Home.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import "./Home.css";
+import { Logo } from "../images/Netflix";
+import { ConnectButton } from "web3uikit";
 
 const Home = () => {
 
 return(
   <>
+  <div className="logo">
+    <Logo />
+  </div>
+  <div className="connect">
+    <ConnectButton />
+  </div>
   <div className="container">
      <Link to="/player" className="link">Player Page</Link>
   </div>
+  
   </>
 )
 }
